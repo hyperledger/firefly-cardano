a9f89ec9f9423f32affe3877ebe578abcab89808
Simon Gellis <simongellis@gmail.com>
2025-01-13 15:09:01 -0500

    feat: extract "shared" components from contracts

--- wasm/simple-tx/src/lib.rs
+++ wasm/simple-tx/src/lib.rs
@@ -1,10 +1,11 @@
 use balius_sdk::{
     txbuilder::{
-        primitives::TransactionInput, AddressPattern, BuildContext, BuildError, FeeChangeReturn,
-        InputExpr, OutputBuilder, TxBuilder, UtxoPattern, UtxoSource,
+        AddressPattern, BuildError, FeeChangeReturn, OutputBuilder, TxBuilder, UtxoPattern,
+        UtxoSource,
     },
     Config, FnHandler, NewTx, Params, Worker, WorkerResult,
 };
+use firefly_balius::CoinSelectionInput;
 use pallas_addresses::Address;
 use serde::Deserialize;
 
@@ -16,53 +17,13 @@ struct TransferRequest {
     pub amount: u64,
 }
 
-struct CoinSelectionInput(UtxoSource, u64);
-
-// TODO: this is a naive approach to coin selection,
-// balius can probably help with a better one
-impl InputExpr for CoinSelectionInput {
-    fn eval(&self, ctx: &BuildContext) -> Result<Vec<TransactionInput>, BuildError> {
-        let utxos = self.0.resolve(ctx)?;
-        // If we know the fee, add it to the target amount.
-        // If not, overestimate the fee so we pick at least as many TXOs as needed.
-        let target_lovelace = self.1
-            + if ctx.estimated_fee == 0 {
-                2_000_000
-            } else {
-                ctx.estimated_fee
-            };
-
-        let mut inputs = vec![];
-        let mut lovelace_so_far = 0;
-        let mut pairs: Vec<_> = utxos.iter().collect();
-        pairs.sort_by_key(|(ref_, _)| (ref_.hash, ref_.index));
-        for (ref_, txo) in pairs {
-            let coin = txo.value().coin();
-            if coin == 0 {
-                continue;
-            }
-            inputs.push(TransactionInput {
-                transaction_id: ref_.hash,
-                index: ref_.index,
-            });
-            lovelace_so_far += coin;
-            if lovelace_so_far >= target_lovelace {
-                break;
-            }
-        }
-        if lovelace_so_far >= target_lovelace {
-            Ok(inputs)
-        } else {
-            Err(BuildError::OutputsTooHigh)
-        }
-    }
-}
-
 fn send_ada(_: Config<()>, req: Params<TransferRequest>) -> WorkerResult<NewTx> {
     let mut tx = TxBuilder::new();
 
-    let from_address = Address::from_bech32(&req.from_address).map_err(|_| BuildError::MalformedAddress)?;
-    let to_address = Address::from_bech32(&req.to_address).map_err(|_| BuildError::MalformedAddress)?;
+    let from_address =
+        Address::from_bech32(&req.from_address).map_err(|_| BuildError::MalformedAddress)?;
+    let to_address =
+        Address::from_bech32(&req.to_address).map_err(|_| BuildError::MalformedAddress)?;
 
     let address_source = UtxoSource::Search(UtxoPattern {
         address: Some(AddressPattern {
